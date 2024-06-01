a73494daa9d11e130bf27ded5ceb0d6b92fcdcbd
FernandoHenning <fernando_hema@hotmail.com>
2024-06-01 13:30:06 -0600

    feat: Identicon generator v1.0
    This commit contains the first version of the generator, which contains
    the code for:
    
    - Get the hash value from the user input.
    - Get the foreground color from the hash value.
    - Generate the grid from the hash value
    - Prints a visual representation of the Identicon into the console.

---
+++ Identicon/Generator/Identicon.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Security.Cryptography;
+
+namespace Generator
+{
+	public class Identicon
+	{
+		private string _input;
+		private byte[] _hash;
+		private int _foregroundColor;
+
+		private readonly int GRID_SIZE = 6;
+		private bool[][] _grid;
+
+
+		public Identicon(string input)
+		{	
+			_input = input;
+			_hash = GetHash();
+			_foregroundColor = GetColorFromHash();
+			_grid = GenerateGridFromHash();
+		}
+
+		public bool[][] GetGrid() => _grid;
+		public int GetForegroundColor() => _foregroundColor;
+		
+		private bool[][] GenerateGridFromHash()
+		{
+			if(_hash.Length <9)
+				throw new ArgumentException("The hash array must contain at least 9 elements");
+
+			var grid = new bool[GRID_SIZE][];
+			
+			for(int i = 3; i < 9 ; i++)
+			{
+				var row = new bool[GRID_SIZE];
+				byte currentByte = _hash[i];
+				for(int bitIndex = 0; bitIndex<3; bitIndex++)
+				{
+					int bitmask = 1 << (7 - bitIndex);
+					row[bitIndex] = (currentByte & bitmask) != 0;
+				}
+
+				for(int bitIndex = row.Length -1 ; bitIndex >= 3; bitIndex--)
+				{
+					row[bitIndex] = row[(row.Length - 1) - bitIndex];
+				}
+				grid[i-3] = row;
+			}
+			return grid;
+		}
+
+		private int GetColorFromHash()
+		{
+			if(_hash.Length <3)
+			       	throw new ArgumentException("The hash array must contain at least 3 elemnnts");
+
+			byte red = _hash[0];
+			byte green = _hash[1];
+			byte blue = _hash[2];
+
+			return (red << 16) | (green << 8) | blue;
+		}
+
+
+		private byte[] GetHash()
+		{
+			using(MD5 hasher = MD5.Create())
+			{
+				Encoding encoding = Encoding.UTF8;
+				return hasher.ComputeHash(encoding.GetBytes(_input));
+			}
+		}
+	}
+}
--- Identicon/Program.cs
+++ Identicon/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Generator;
+
+using Spectre.Console;
+
+var userInput = PromptInput();
+var identicon = new Identicon(userInput);
+
+PrintIdenticon(identicon.GetGrid(), identicon.GetForegroundColor());
+
+string PromptInput()
+{
+	var input = AnsiConsole.Ask<string>("What's your [green]email? [/]");
+	return input;
+}
+
+void PrintIdenticon(bool[][] grid, int foregroundColor)
+{
+	var canvas = new Canvas(6,6);
+
+	byte red = (byte) ((foregroundColor >> 16) & 0xff);
+	byte green = (byte) ((foregroundColor >> 8) & 0xff);
+	byte blue = (byte) (foregroundColor & 0xff);
+	Color color = new Color(red, green, blue);
+
+	for(int row = 0; row < 6; row ++)
+	{
+		for(int col = 0; col < 6; col++)
+		{
+			if(grid[col][row])
+			{
+				canvas.SetPixel(row, col, color);
+			}
+			else
+			{	
+				canvas.SetPixel(row, col, Color.White);
+			}
+		}
+	}
+	AnsiConsole.Write(canvas);
+}
+
+
