c911396ae742732c4dc8d06dc8e02ecee77a3769
FernandoHenning <fernando_hema@hotmail.com>
2024-06-05 13:37:04 -0600

    refactor: Improve readability for the Identicon.cs file
    This commit contains the following changes to improve readability:
    - Add XML tags to document each function in the file.
    - Refactor the GetGrid, GetForegroundColor and GetHash methods to be
      properties.
    - Rename GetInputHash method to ComputeHash, and change the way of call
      the hash function.
    - Add more validation for the arguments of the GenerateGridFromHash and
      form the parameter "input" of the class constructor.

--- Identicon/Generator/Identicon.cs
+++ Identicon/Generator/Identicon.cs
@@ -1,78 +1,95 @@
-using System;
 using System.Text;
-using System.Collections;
 using System.Security.Cryptography;
 
 namespace Generator
 {
-	public class Identicon
-	{
-		private string _input;
-		private byte[] _hash;
-		private int _foregroundColor;
+    public class Identicon
+    {
+        private readonly string _input;
+        private readonly byte[] _hash;
+        private readonly int _foregroundColor;
+        private readonly bool[][] _grid;
 
-		private readonly int GRID_SIZE = 6;
-		private bool[][] _grid;
+        public Identicon(string input)
+        {
+            _input = input ?? throw new ArgumentNullException(nameof(input));
+            _hash = ComputeHash(_input);
+            _foregroundColor = GetColorFromHash(_hash);
+            _grid = GenerateGridFromHash(_hash);
+        }
 
+        public bool[][] Grid => _grid;
+        public int ForegroundColor => _foregroundColor;
+        public byte[] Hash => _hash;
 
-		public Identicon(string input)
-		{	
-			_input = input;
-			_hash = GetInputHash();
-			_foregroundColor = GetColorFromHash();
-			_grid = GenerateGridFromHash();
-		}
+        /// <summary>
+        /// Generates a grid of boolean values representing the identicon.
+        /// </summary>
+        /// <param name="hash">The hash value to generate the grid from.</param>
+        /// <returns>A bidimensional array representing the grid.</returns>
+        /// <exception cref="ArgumentException">Thrown when the hash array is not valid.</exception>
+        /// <exception cref="ArgumentnullException">Thrown when the hash array is null.</exception>
+        private static bool[][] GenerateGridFromHash(byte[] hash)
+        {
+            const int RequiredHashLength = 9;
+            const int GridSize = 6; // Number of rows/columns in the grid
+            const int BitsToExtract = 3;
 
-		public bool[][] GetGrid() => _grid;
-		public int GetForegroundColor() => _foregroundColor;
-		public byte[] GetHash() => _hash;
+            if (hash == null)
+                throw new ArgumentNullException(nameof(hash), "The hash array cannot be null.");
 
-		private bool[][] GenerateGridFromHash()
-		{
-			if(_hash.Length <9)
-				throw new ArgumentException("The hash array must contain at least 9 elements");
+            if (hash.Length < RequiredHashLength)
+                throw new ArgumentException($"The hash array must contain at least {RequiredHashLength} elements", nameof(hash));
 
-			var grid = new bool[GRID_SIZE][];
-			
-			for(int i = 3; i < 9 ; i++)
-			{
-				var row = new bool[GRID_SIZE];
-				byte currentByte = _hash[i];
-				for(int bitIndex = 0; bitIndex<3; bitIndex++)
-				{
-					int bitmask = 1 << (7 - bitIndex);
-					row[bitIndex] = (currentByte & bitmask) != 0;
-				}
+            var grid = new bool[GridSize][];
 
-				for(int bitIndex = row.Length -1 ; bitIndex >= 3; bitIndex--)
-				{
-					row[bitIndex] = row[(row.Length - 1) - bitIndex];
-				}
-				grid[i-3] = row;
-			}
-			return grid;
-		}
+            for (int i = 0; i < GridSize; i++)
+            {
+                var row = new bool[GridSize];
+                byte currentByte = hash[i + 3];
 
-		private int GetColorFromHash()
-		{
-			if(_hash.Length <3)
-			       	throw new ArgumentException("The hash array must contain at least 3 elemnnts");
+                for (int bitIndex = 0; bitIndex < BitsToExtract; bitIndex++)
+                {
+                    int bitmask = 1 << (7 - bitIndex);
+                    row[bitIndex] = (currentByte & bitmask) != 0;
+                }
 
-			byte red = _hash[0];
-			byte green = _hash[1];
-			byte blue = _hash[2];
+                for (int bitIndex = BitsToExtract; bitIndex < GridSize; bitIndex++)
+                {
+                    row[bitIndex] = row[GridSize - 1 - bitIndex];
+                }
 
-			return (red << 16) | (green << 8) | blue;
-		}
+                grid[i] = row;
+            }
 
+            return grid;
+        }
 
-		private byte[] GetInputHash()
-		{
-			using(MD5 hasher = MD5.Create())
-			{
-				Encoding encoding = Encoding.UTF8;
-				return hasher.ComputeHash(encoding.GetBytes(_input));
-			}
-		}
-	}
+        /// <summary>
+        /// Returns a color, represented as an integter, from the first 3 digits of the hash value.
+        /// </summary>
+        /// <param name="hash">The hash value to generate the grid from.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <returns>An integer representing a color.</returns>
+        private static int GetColorFromHash(byte[] hash)
+        {
+            if (hash.Length < 3)
+                throw new ArgumentException("The hash array must contain at least 3 elements", nameof(hash));
+
+            byte red = hash[0];
+            byte green = hash[1];
+            byte blue = hash[2];
+
+            return (red << 16) | (green << 8) | blue;
+        }
+        /// <summary>
+        /// Compute the hash from the input. Uses the MD5 hash function.
+        /// </summary>
+        /// <param name="input">The string input to hash.</param>
+        /// <returns>A byte array with the hash value.</returns>
+        private static byte[] ComputeHash(string input)
+        {
+            return MD5.HashData(Encoding.UTF8.GetBytes(input));
+        }
+    }
 }
